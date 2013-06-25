be9c9d97ef9c988099814cc96d945a7a3817bd94
Michael <Michael.Eberspaecher@mhm-hr.com>
2013-06-25 12:29:54 +0200

    Erstes Setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SurveyTool::Application.config.secret_token = 'aee5838f02f49743e40484cae641ce60c6e30ac097f937c259071089de9713fae2dbac9a5df733dd67cd8ca204b79f6822a1b2fe05492d242234984ebbe90d8f'
